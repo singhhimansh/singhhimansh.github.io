fe0a443a179ef86ac11f1eae9894b335d5925e8d
singhhimansh <him.signum802@gmail.com>
2022-08-16 16:57:36 +0530

    updated urls

--- src/components/Pallets.jsx
+++ src/components/Pallets.jsx
@@ -15,9 +15,9 @@ function Tabs(){
                 { projects.map((project)=>{
 
                     return(
-                        <div key={project.id} className={`group my-24 md:my-32 lg:mx-10 h-96 relative md:grid md:grid-cols-12 gap-3 items-center select-none ${ project.id % 2 === 1 ? "text-right" : "text-left" } `}>
+                        <div key={project.id} className={`group items-center my-24 md:my-32 lg:mx-10 h-96 relative md:grid md:grid-cols-12 gap-3  select-none ${ project.id % 2 === 1 ? "text-right" : "text-left" } `}>
                          
-                           <div className={`z-[1] p-5 md:p-0 h-[90%]  bg-[#112240]/90 backdrop-blur-[3px] md:backdrop-blur-none md:bg-transparent absolute top-0 left-0 md:relative flex flex-col justify-center border md:border-none rounded-md border-emerald-200/40 w-full  ${ project.id % 2 === 1 ? "md:col-span-8  lg:col-span-6 md:col-end-13 lg:col-end-13" :"md:col-span-8 lg:col-span-6 md:col-start-1 "  }`}>
+                           <div className={`z-[1] p-5 md:p-0 h-[90%]   bg-[#112240]/90 backdrop-blur-[3px] md:backdrop-blur-none md:bg-transparent absolute top-0 left-0 md:relative flex flex-col justify-center border md:border-none rounded-md border-emerald-200/40 w-full  ${ project.id % 2 === 1 ? "md:col-span-8  lg:col-span-6 md:col-end-13 lg:col-end-13" :"md:col-span-8 lg:col-span-6 md:col-start-1 "  }`}>
                                 <h1 className="group-hover:text-teal-400 font-bold tracking-wider text-2xl md:text-3xl text-lightestslate hover:text-teal-400 duration-200">{project.title}</h1>
                                 <h2 className="group-hover:text-amber-200 duration-200  mt-1 mb-4 text-base text-teal-400/80">{project.subtitle}</h2>
                                 <p className="z-[1] text-lg my-4 md:p-5 md:bg-[#112240]/90 rounded-md text-lightslate font-calibre  md:backdrop-blur-sm">{project.description}</p>
@@ -38,7 +38,7 @@ function Tabs(){
                             </div>
                            
                             {/* <div className= "overflow-hidden rounded-md"> */}
-                            <img className={`absolute top-0 left-0 z-0 h-[90%] rounded-lg object-fill saturate-0 hover:saturate-100 duration-200 self-center ${ project.id % 2 === 1 ? "md:col-start-1 md:col-span-8" : "md:col-start-5 md:col-span-8" }`} src={require(`${project.img}`)} alt="" />
+                            <img className={`absolute md:h-4/5 lg:h-full top-0 md:top-10 lg:top-0 left-0 z-0 h-[90%] rounded-lg object-fill saturate-0 hover:saturate-100 duration-200 self-center ${ project.id % 2 === 1 ? "md:col-start-1 md:col-span-8" : "md:col-start-5 md:col-span-8" }`} src={require(`${project.img}`)} alt="" />
                             {/* </div> */}
 
 
